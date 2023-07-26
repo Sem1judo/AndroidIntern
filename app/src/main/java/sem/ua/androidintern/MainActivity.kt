491305f91bc4fba765827631026b22abe62aab1e
sem <judo1stss1@gmail.com>
2023-07-26 14:56:40 +0300

    level_1 (middle) task done

--- app/src/main/java/sem/ua/androidintern/MainActivity.kt
+++ app/src/main/java/sem/ua/androidintern/MainActivity.kt
@@ -1,11 +1,42 @@
 package sem.ua.androidintern
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import sem.ua.androidintern.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var counter = 0
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState != null) {
+            counter = savedInstanceState.getInt("counter", 0)
+        }
+        updateCounterText()
+        binding.counterBtn.setOnClickListener {
+            counter++
+            updateCounterText()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("counter", counter)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        counter = savedInstanceState.getInt("counter", 0)
+        updateCounterText()
+    }
+
+    private fun updateCounterText() {
+        val counterText = resources.getString(R.string.counter, counter)
+        binding.counterTv.text = counterText
     }
 }
